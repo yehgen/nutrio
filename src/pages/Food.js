48fa645c40d0e4a5ebf15a615b8b0274e50bf8fd
yehgen <yehgen@uw.edu>
2022-02-28 12:07:17 -0800

    add search to food page

--- src/pages/Food.js
+++ src/pages/Food.js
@@ -1,6 +1,48 @@
 // for searching or tracking food items
+import '../App.css';
+import '../Food.css';
+import { useState } from "react";
+import * as React from 'react';
+
+// Components
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 const Food = () => {
-  return <h1>Food</h1>;
-};
+  const [food, setFood] = useState("")
+  const [inputError, setInputError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (food == "") {
+      setInputError(true);
+    }
+    if (food) {
+      setInputError(false)
+      // setFood("");
+      console.log(food);
+      
+    }
+  }
+
+  return (
+  <div className="App-left">
+    <h1>Add Food Intake</h1>
+    <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+      <div className="input-field">
+        <TextField
+          onChange={(e) => setFood(e.target.value)}
+          id="standard-basic"
+          label="food"
+          variant="standard"
+          fullWidth
+          error={inputError}
+        />
+        <Button variant="contained" onClick={handleSubmit}>Search</Button>
+      </div> 
+    </form>
+  </div>
+  );
+}
 
 export default Food;
